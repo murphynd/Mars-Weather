e75a701e989b2740b7daff9d8f0e8ca3deec6d1f
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-10 12:03:26 -0700

    adds form to html and form interface to UI in javascript

--- src/main.js
+++ src/main.js
@@ -2,52 +2,57 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import { round } from "./js/two.js";
+// import { round } from "./js/two.js";
 // import * as oneFunctions from "./js/one.js";
 // example function call: oneFunctions.temp();
 
+// $(document).ready(function () {
+//   let request = new XMLHttpRequest();
+//   const url = `https://api.nasa.gov/insight_weather/?api_key=${process.env.API_KEY}&feedtype=json&ver=1.0`;
+//   request.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//       const response = JSON.parse(this.responseText);
+//       console.log(response);
+//       getElements(response);
+//     }
+//   };
+//   request.open("GET", url, true);
+//   request.send();
+//   function getElements(response) {
+//     let currentDay = `${response.sol_keys[1]}`;
+//     let earthdate = new Date(`${response[currentDay].First_UTC}`);
+//     $(".Temp").text(`${round(response[currentDay].AT.av)}°F`);
+//     $(".High").text(`${round(response[currentDay].AT.mx)}°F`);
+//     $(".Low").text(`${round(response[currentDay].AT.mn)}°F`);
+//     $(".Earthdate").text(earthdate);
+//   }
+// });
 $(document).ready(function () {
-  let request = new XMLHttpRequest();
-  const url = `https://api.nasa.gov/insight_weather/?api_key=${process.env.API_KEY}&feedtype=json&ver=1.0`;
-
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
-      getElements(response);
+  $("form#rover").submit(function (event) {
+    event.preventDefault();
+    let date = $("#dateinput").val();
+    // let roverName = $("input:checkbox[name=rovers]:checked");
+    let request = new XMLHttpRequest();
+    let url2 = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${process.env.API_KEY}`;
+    // console.log(roverName);
+    console.log(date);
+    console.log(url2);
+    // yyyy-mm-dd
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        console.log(response);
+        getElements(response);
+      }
+    };
+    request.open("GET", url2, true);
+    request.send();
+    function getElements(response) {
+      $("#imageStatus").text("Found");
+      $("#imageID").src = `${response.photos[0].img_src}`;
+      $("#roverCaption").text(`${response.photos[0].rover.name}`);
+      $("#landingCaption").text(`${response.photos[0].rover.landing_date}`);
+      $("#endingCaption").text(`${response.photos[0].rover.max_date}`);
     }
-  };
-
-  request.open("GET", url, true);
-  request.send();
-
-  function getElements(response) {
-    let currentDay = `${response.sol_keys[1]}`;
-    let yesterday = `${response.sol_keys[0]}`;
-
-    console.log(currentDay);
-
-    `${response[currentDay].AT.av}`;
-    console.log(`${response[currentDay].AT.av}`);
-
-    $(".Temp").text(
-      `Average temp on Mars today ${round(response[currentDay].AT.av)}°F
-      with a High of ${round(
-        response[currentDay].AT.mx
-      )}°F and a low of ${round(response[currentDay].AT.mn)}°F`
-    );
-    $(".Earthdate").text(
-      `Todays date on Earth is ${Date(response[currentDay].First_UTC)}`
-    );
-    $(".YTemp").text(
-      `Average temp on Mars yesterday ${round(response[yesterday].AT.av)}°F
-      with a High of ${round(response[yesterday].AT.mx)}°F and a low of ${round(
-        response[yesterday].AT.mn
-      )}°F`
-    );
-    $(".YEarthdate").text(
-      `Yesterday date on Earth is ${Date(response[yesterday].First_UTC)}`
-    );
-    console.log(`${response[yesterday].First_UTC}`);
-  }
+  });
 });
